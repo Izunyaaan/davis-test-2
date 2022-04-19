fa338d0347c57bdd3b73726a1ddc2078928ef16d
Izunyaaan <kanebluerivers@gmail.com>
2022-04-19 11:30:21 +0800

    UI frameworks done. Q1 answered. Starting Q2

---
+++ yourcode.js
@@ -0,0 +1,106 @@
+import {MnistData, IMAGE_H, IMAGE_W} from './data.js';
+
+function createConvModel() {
+  const model = tf.sequential();
+  model.add(tf.layers.conv2d({
+    inputShape: [IMAGE_H, IMAGE_W, 1],
+    kernelSize: 3,
+    filters: 16,
+    activation: 'relu'
+  }));
+
+  //model.add();
+  //model.add();
+  model.add(tf.layers.dense({units: 10, activation: 'softmax'}));
+  return model;
+}
+
+
+function createDenseModel() {
+  const model = tf.sequential();
+  model.add(tf.layers.flatten({inputShape: [IMAGE_H, IMAGE_W, 1]}));
+  //model.add();
+  model.add(tf.layers.dense({units: 10, activation: 'softmax'}));
+  return model;
+}
+
+async function train(model, onIteration) {
+  //const optimizer = ...
+  model.compile({
+    optimizer,
+    loss: 'categoricalCrossentropy',
+    metrics: ['accuracy'],
+  });
+
+  const batchSize = 320;
+  const validationSplit = 0.15;
+  const trainEpochs = 3;
+  let trainBatchCount = 0;
+
+  const trainData = data.getTrainData();
+  const testData = data.getTestData();
+
+  const totalNumBatches =
+      Math.ceil(trainData.xs.shape[0] * (1 - validationSplit) / batchSize) *
+      trainEpochs;
+
+  let valAcc;
+  await model.fit(trainData.xs, trainData.labels, {
+    batchSize,
+    validationSplit,
+    epochs: trainEpochs,
+    callbacks: {
+      onBatchEnd: async (batch, logs) => {
+        trainBatchCount++;
+        console.log(`Training... (` +
+            `${(trainBatchCount / totalNumBatches * 100).toFixed(1)}%` +
+            ` complete). To stop training, refresh or close page.`);
+        //
+        if (onIteration && batch % 10 === 0) {
+          onIteration('onBatchEnd', batch, logs);
+        }
+        await tf.nextFrame();
+      },
+      onEpochEnd: async (epoch, logs) => {
+        valAcc = logs.val_acc;
+        //
+        if (onIteration) {
+          onIteration('onEpochEnd', epoch, logs);
+        }
+        await tf.nextFrame();
+      }
+    }
+  });
+
+  const testResult = model.evaluate(testData.xs, testData.labels);
+  const testAccPercent = testResult[1].dataSync()[0] * 100;
+  const finalValAccPercent = valAcc * 100;
+    console.log(
+      `Final validation accuracy: ${finalValAccPercent.toFixed(1)}%; ` +
+      `Final test accuracy: ${testAccPercent.toFixed(1)}%`);
+}
+
+async function showPredictions(model) {
+  const testExamples = 100;
+  const examples = data.getTestData(testExamples);
+
+  tf.tidy(() => {
+    const output = model.predict(examples.xs);
+    const axis = 1;
+    const labels = Array.from(examples.labels.argMax(axis).dataSync());
+    const predictions = Array.from(output.argMax(axis).dataSync());
+
+  });
+}
+
+
+let data;
+async function load() {
+    data = new MnistData();
+    await data.load();
+    const model = createModel();
+    model.summary();
+    await train(model, () => showPredictions(model));
+}
+
+await load();
